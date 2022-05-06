26d8d5878571387bff942b0dc3fa33ea543b4620
qinghuanI <1062217965@qq.com>
2022-05-06 19:51:27 +0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isDev = process.env.NODE_ENV === 'development'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: isDev ? '/': '/deploy-react',
-})
+export default ({mode}) => {
+  return defineConfig({
+    plugins: [react()],
+    base: mode ? '/': '/deploy-react',
+  })
+}
