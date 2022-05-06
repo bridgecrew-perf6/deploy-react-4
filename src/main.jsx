545f23cd10bf9ac955a3f2fcd32e9c1f28d0c2fb
qinghuanI <1062217965@qq.com>
2022-05-06 20:11:34 +0800

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {HashRouter as Router} from "react-router-dom";
+
+import App from './App';
+
 import './index.css'
 
-import { HashRouter as Router} from "react-router-dom";
+const isDev = import.meta.env.MODE === 'development';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Router>
-          <App />
-      </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router basename={isDev ? '/' : '/deploy-react'}>
+            <App/>
+        </Router>
+    </React.StrictMode>
 )
