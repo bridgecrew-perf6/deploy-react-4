cffd6f5db2e47662709825641a1aa03d530b1e8e
qinghuanI <1062217965@qq.com>
2022-05-06 20:24:10 +0800

    add router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ import Page1 from "../pages/page1";
 import { Navigate } from "react-router-dom";
 
 export const routes = [
-    {path:'/', element: <Navigate to='/page1'/>},
+    {path:'/', exact: true, element: <Navigate to='/page1'/>},
     {path:'/page1',exact: true, element: <Page1/>},
     {path:'/page2', exact: true, element: <Page2/>},
 ]
